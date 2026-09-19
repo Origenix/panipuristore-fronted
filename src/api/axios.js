1e1179c01a69cc2803fc0e3a604ec78c395bd665
Origenix <support.origenix@gmail.com>
2026-09-19 18:59:05 +0530

    Allow configurable frontend API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://panipuristore.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://panipuristore.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
