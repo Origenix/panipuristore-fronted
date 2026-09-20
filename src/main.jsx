dda3999d5271e16055d54825cf42b3b603762e99
Origenix <support.origenix@gmail.com>
2026-09-20 13:35:26 +0530

    Enable global duplicate-click protection

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import { AuthProvider } from './context/AuthContext'
 import { CartProvider } from './context/CartContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import GlobalButtonGuard from './components/GlobalButtonGuard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="1006796195544-b54d8jrh4d08udaia0ehrpj5l0f3v1va.apps.googleusercontent.com">
       <ThemeProvider>
+        <GlobalButtonGuard />
         <AuthProvider>
           <CartProvider>
             <App />
