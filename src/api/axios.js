fdccccdd6cc9faa92c9fa455b462f7de25737059
Origenix <support.origenix@gmail.com>
2026-09-20 13:35:01 +0530

    Track API request lifecycle for button locking

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,8 +6,25 @@ const axiosInstance = axios.create({
   baseURL: API_URL,
 });
 
+let activeRequestCount = 0;
+
+const notifyRequestStart = () => {
+  activeRequestCount += 1;
+  window.dispatchEvent(new CustomEvent('panipuri:api-start', {
+    detail: { activeRequestCount },
+  }));
+};
+
+const notifyRequestEnd = () => {
+  activeRequestCount = Math.max(0, activeRequestCount - 1);
+  window.dispatchEvent(new CustomEvent('panipuri:api-end', {
+    detail: { activeRequestCount },
+  }));
+};
+
 axiosInstance.interceptors.request.use(
   (config) => {
+    notifyRequestStart();
     const token = localStorage.getItem("token");
     if (token) {
       config.headers["Authorization"] = "Bearer " + token;
@@ -15,6 +32,18 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error) => {
+    notifyRequestEnd();
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    notifyRequestEnd();
+    return response;
+  },
+  (error) => {
+    notifyRequestEnd();
     return Promise.reject(error);
   },
 );
